02c540b2b8fd72305b9794a9493e6a050b16e14b
Denis <example@gmail.com>
2022-08-05 23:19:04 +0700

    js-burger

--- src/js/app.js
+++ src/js/app.js
@@ -75,3 +75,12 @@ let currentScrollPos = window.pageYOffset;
 
 
 
+const iconMenu = document.querySelector('.icon-menu');
+const menuBody = document.querySelector('.header__menu');
+if (iconMenu) {
+    iconMenu.addEventListener("click", function (e) {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
+}
